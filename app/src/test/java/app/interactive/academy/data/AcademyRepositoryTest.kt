1e38262c7261f3fe2b3d140fe83194f39018a770
nizam_interactive <developer_004@interactive.co.id>
2019-07-10 15:36:05 +0700

    All unit tests are passed

--- app/src/test/java/app/interactive/academy/data/AcademyRepositoryTest.kt
+++ app/src/test/java/app/interactive/academy/data/AcademyRepositoryTest.kt
@@ -6,10 +6,11 @@ import app.interactive.academy.data.dummy.generateRemoteModules
 import app.interactive.academy.data.dummy.getRemoteContent
 import app.interactive.academy.data.source.local.LocalRepository
 import app.interactive.academy.data.source.remote.RemoteRepository
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 
 /**
  * Created by L
@@ -30,6 +31,47 @@ class AcademyRepositoryTest{
 
     @Test
     fun testGetAllCourses(){
-//        `when`()
+        `when`(remote.getAllCourses()).thenReturn(courseResponses)
+        val courseEntities=academyRepository.getAllCourses()
+        verify(remote).getAllCourses()
+        assertNotNull(courseEntities)
+        assertEquals(courseResponses.size,courseEntities.size)
+    }
+
+    @Test
+    fun testetAllModulesByCourse(){
+        `when`(remote.getAllModules(courseId)).thenReturn(moduleResponses)
+        val modules=academyRepository.getAllModulesByCourse(courseId)
+        verify(remote).getAllModules(courseId)
+        assertNotNull(modules)
+        assertEquals(moduleResponses.size,modules.size)
+    }
+
+    @Test
+    fun testGetBookmarkedCourses(){
+        `when`(remote.getAllCourses()).thenReturn(courseResponses)
+        val courses=academyRepository.getBookmarkedCourses()
+        verify(remote).getAllCourses()
+        assertNotNull(courses)
+        assertEquals(courseResponses.size,courses.size)
+    }
+
+    @Test
+    fun testGetContent(){
+        `when`(remote.getAllModules(courseId)).thenReturn(moduleResponses)
+        `when`(remote.getContent(moduleId)).thenReturn(content)
+        val resultModule=academyRepository.getContent(courseId,moduleId)
+        verify(remote).getContent(moduleId)
+        assertNotNull(resultModule)
+        assertEquals(content.content,resultModule?.contentEntity?.content)
+    }
+
+    @Test
+    fun testGetCourseWithModules(){
+        `when`(remote.getAllCourses()).thenReturn(courseResponses)
+        val resultCourse=academyRepository.getCourseWithModule(courseId)
+        verify(remote).getAllCourses()
+        assertNotNull(resultCourse)
+        assertEquals(courseResponses[0].title,resultCourse?.title)
     }
 }
--- app/src/test/java/app/interactive/academy/data/FakeAcademyRepository.kt
+++ app/src/test/java/app/interactive/academy/data/FakeAcademyRepository.kt
@@ -1,13 +1,115 @@
 package app.interactive.academy.data
 
 import app.interactive.academy.data.source.local.LocalRepository
+import app.interactive.academy.data.source.local.entity.ContentEntity
+import app.interactive.academy.data.source.local.entity.CourseEntity
+import app.interactive.academy.data.source.local.entity.ModuleEntity
 import app.interactive.academy.data.source.remote.RemoteRepository
 
 /**
  * Created by L
  *
- * on 7/9/2019
+ * on 7/8/2019
  */
-class FakeAcademyRepository(private val localRepository: LocalRepository,private val remoteRepository: RemoteRepository){
+class FakeAcademyRepository(private val localRepository: LocalRepository, private val remoteRepository: RemoteRepository) :
+    AcademyDataSource {
 
+    companion object {
+        private var INSTANCE: FakeAcademyRepository?=null
+
+        fun getInstance(localRepository: LocalRepository, remoteRepository: RemoteRepository): FakeAcademyRepository {
+            if (INSTANCE == null) {
+                synchronized(FakeAcademyRepository::class.java) {
+                    if (INSTANCE == null) {
+                        INSTANCE =
+                            FakeAcademyRepository(localRepository, remoteRepository)
+                    }
+                }
+            }
+            return INSTANCE as FakeAcademyRepository
+        }
+    }
+
+    override fun getAllCourses(): List<CourseEntity> {
+        return ArrayList<CourseEntity>().also {
+            remoteRepository.getAllCourses().forEach { course ->
+                it.add(
+                    CourseEntity(
+                        course.id,
+                        course.title,
+                        course.description,
+                        course.date,
+                        course.imagePath
+                    )
+                )
+            }
+        }
+    }
+
+    override fun getCourseWithModule(courseId: String): CourseEntity? {
+        var course: CourseEntity? = null
+        remoteRepository.getAllCourses().forEach {
+            if (it.id == courseId) {
+                course = CourseEntity(
+                    it.id,
+                    it.title,
+                    it.description,
+                    it.date,
+                    it.imagePath
+                )
+            }
+        }
+        return course
+    }
+
+    override fun getAllModulesByCourse(courseId: String): ArrayList<ModuleEntity> {
+        return ArrayList<ModuleEntity>().also {
+            remoteRepository.getAllModules(courseId).forEach { module ->
+                it.add(
+                    ModuleEntity(
+                        module.moduleId,
+                        module.courseId,
+                        module.title,
+                        module.position
+                    )
+                )
+            }
+        }
+    }
+
+    override fun getBookmarkedCourses(): ArrayList<CourseEntity> {
+        return ArrayList<CourseEntity>().also {
+            remoteRepository.getAllCourses().forEach { course ->
+                it.add(
+                    CourseEntity(
+                        course.id,
+                        course.title,
+                        course.description,
+                        course.date,
+                        course.imagePath
+                    )
+                )
+            }
+        }
+    }
+
+    override fun getContent(courseId: String, moduleId: String): ModuleEntity? {
+        var moduleEntity: ModuleEntity? = null
+        remoteRepository.getAllModules(courseId).forEach {
+            if (it.moduleId == moduleId) {
+                moduleEntity = ModuleEntity(
+                    it.moduleId,
+                    it.courseId,
+                    it.title,
+                    it.position,
+                    remoteRepository.getContent(moduleId)?.content?.let { content ->
+                        ContentEntity(
+                            content
+                        )
+                    }
+                )
+            }
+        }
+        return moduleEntity
+    }
 }
